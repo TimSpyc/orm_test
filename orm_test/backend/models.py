67d1e2417acc51ab47bf3f082075af5c0c90d08d
TimSpyc <89732871+TimSpyc@users.noreply.github.com>
2023-06-04 12:43:44 +0000

    CacheIntermediate fixed

--- orm_test/backend/models.py
+++ orm_test/backend/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.db.models import Max, Subquery
+from django.db.models import Max, Subquery, Q
 from django.core.validators import MinValueValidator, MaxValueValidator
-import pickle
-
+import pickle, json
+from datetime import datetime
+from datetime import datetime
 
 class GroupTable(models.Model):
     """
@@ -21,7 +22,8 @@ class ReferenceTable(models.Model):
 
 class User(ReferenceTable):
     """
-    A Django model representing a User with a Microsoft ID, name, last name, email, and last login date.
+    A Django model representing a User with a Microsoft ID, name, last name,
+    email, and last login date.
     """
     microsoft_id = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
@@ -35,7 +37,8 @@ class User(ReferenceTable):
 
 class DataTable(models.Model):
     """
-    An abstract Django model for representing data tables, including date, creator, and active status.
+    An abstract Django model for representing data tables, including date,
+    creator, and active status.
     """
     date = models.DateTimeField()
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -45,7 +48,8 @@ class DataTable(models.Model):
 
 class CacheManager(models.Model):
     """
-    A Django model representing cache entries, which store the manager name, group ID, date, and pickled data.
+    A Django model representing cache entries, which store the manager name,
+    group ID, date, and pickled data.
     """
     manager_name = models.CharField(max_length=100)
     group_id = models.IntegerField()
@@ -56,9 +60,18 @@ class CacheManager(models.Model):
         unique_together = ('manager_name', 'group_id', 'date')
 
     @classmethod
-    def get_cache_data(cls, manager_name, group_model_obj, group_model_name, data_model, input_group_id, date):
+    def get_cache_data(
+        cls,
+        manager_name: str,
+        group_model_obj: models.Model,
+        group_model_name: str,
+        data_model: models.Model,
+        input_group_id: int,
+        date: datetime | None
+    ) -> object | None:
         """
-        Retrieve cached data for a given manager name, group model object, group model name, data model, input group ID, and date.
+        Retrieve cached data for a given manager name, group model object,
+        group model name, data model, input group ID, and date.
         
         Args:
             manager_name (str): The name of the manager.
@@ -66,10 +79,12 @@ class CacheManager(models.Model):
             group_model_name (str): The name of the group model.
             data_model (models.Model): The data model.
             input_group_id (int): The input group ID.
-            date (datetime.datetime): The date used for filtering the cached data. If set to None, the latest date will be used.
+            date (datetime.datetime): The date used for filtering the cached
+                data. If set to None, the latest date will be used.
 
         Returns:
-            object: The cached data as a Python object, or None if no cache entry is found.
+            object: The cached data as a Python object, or None if no cache
+            entry is found.
         """
         try:
             if date is None:
@@ -96,20 +111,28 @@ class CacheManager(models.Model):
         return None
 
     @classmethod
-    def set_cache_data(cls, manager_name, group_id, data, date):
+    def set_cache_data(
+        cls,
+        manager_name: str,
+        group_id: int,
+        data: object,
+        date: datetime
+    ) -> None:
         """
-        Store data in the cache for a given manager name, group ID, data, and date.
+        Store data in the cache for a given manager name, group ID, data, and
+        date.
 
         Args:
             manager_name (str): The name of the manager.
             group_id (int): The group ID.
-            data (object): The data to be stored in the cache as a Python object.
+            data (object): The data to be stored in the cache as a Python
+                object.
             date (datetime.datetime): The date for the cache entry.
 
         Returns:
             None
         """
-        entry, newly_created = cls.objects.get_or_create(
+        entry, _ = cls.objects.get_or_create(
             manager_name=manager_name,
             group_id=group_id,
             date=date
@@ -117,101 +140,103 @@ class CacheManager(models.Model):
         entry.data=pickle.dumps(data)
         entry.save()
 
-class ScenarioGroup(GroupTable):
-    """
-    A Django model representing a scenario group.
-    """
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.id
-
-
-class Scenario(DataTable):
-    """
-    A Django model representing a scenario, including its name, data, and associated scenario group.
-    """
-    name = models.CharField(max_length=255)
-    data = models.JSONField()
-
-    def __str__(self):
-        return self.name
 
 class CacheIntermediate(models.Model):
     """
-    A Django model representing cache entries, which store the intermediate name, scenario, date, and pickled data.
+    A Django model representing cache entries, which store the
+    intermediate name, identification, start_date, end_date and pickled data.
     """
     intermediate_name = models.CharField(max_length=100)
-    relevant_scenario_dict = models.JSONField()
+    identification = models.JSONField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(null=True)
     data = models.BinaryField()
 
     class Meta:
-        unique_together = ('intermediate_name', 'relevant_scenario_dict', 'start_date', 'end_date')
+        unique_together = (
+            'intermediate_name',
+            'identification',
+            'start_date',
+            'end_date'
+        )
 
-    # @classmethod
-    # def get_cache_data(cls, intermediate_name, group_model_obj, input_group_id, date):
-    #     """
-    #     Retrieve cached data for a given manager name, group model object, group model name, data model, input group ID, and date.
-        
-    #     Args:
-    #         manager_name (str): The name of the manager.
-    #         group_model_obj (models.Model): The group model object.
-    #         group_model_name (str): The name of the group model.
-    #         data_model (models.Model): The data model.
-    #         input_group_id (int): The input group ID.
-    #         date (datetime.datetime): The date used for filtering the cached data. If set to None, the latest date will be used.
+    @classmethod
+    def get_cache_data(
+        cls,
+        intermediate_name: str,
+        identification_dict: dict,
+        date: datetime | None
+    ) -> object | None:
+        """
+        Get cached data based on intermediate_name, identification_dict, and
+        date. If date is None, use the current datetime.
 
-    #     Returns:
-    #         object: The cached data as a Python object, or None if no cache entry is found.
-    #     """
-    #     try:
-    #         if date is None:
-    #             latest_dates = data_model.objects.filter(
-    #                 **{group_model_name: group_model_obj}
-    #             ).values(group_model_name).annotate(latest_date=Max('date'))
-    #         else:
-    #             latest_dates = data_model.objects.filter(
-    #                 date__lt=date,
-    #                 group_model_name=group_model_obj
-    #             ).values(group_model_name).annotate(latest_date=Max('date'))
+        Args:
+            intermediate_name (str): The intermediate name.
+            identification_dict (dict): The identification dictionary.
+            date (datetime | None): The datetime to filter the data.
 
-    #         result = cls.objects.filter(
-    #             manager_name=manager_name,
-    #             date=latest_dates.values('latest_date')[:1],
-    #             group_id=input_group_id
-    #         )
-    #         if result.exists():
-    #             entry = result.first()
-    #             return pickle.loads(entry.data)
-    #     except cls.DoesNotExist:
-    #         pass
-    #     except data_model.DoesNotExist:
-    #         raise ValueError(f'data_model {data_model} is not valid')
-    #     return None
+        Returns:
+            object: The cached data if found, None otherwise.
+        """
+        if date is None:
+            date = datetime.now()
+        try:
+            result = cls.objects.filter(
+                Q(start_date__lte=date, end_date__gte=date) | 
+                Q(start_date__lte=date, end_date__isnull=True),
+                intermediate_name=intermediate_name,
+                identification = cls.getIdString(identification_dict),
+            )
+            if result.exists():
+                entry = result.first()
+                return pickle.loads(entry.data)
+        except cls.DoesNotExist:
+            pass
+        return None
 
-    # @classmethod
-    # def set_cache_data(cls, manager_name, group_id, data, date):
-    #     """
-    #     Store data in the cache for a given manager name, group ID, data, and date.
+    @classmethod
+    def set_cache_data(
+        cls,
+        intermediate_name: str,
+        identification_dict: dict,
+        data: object,
+        start_date: datetime,
+        end_date: datetime | None
+    ) -> None:
+        """
+        Set cached data based on intermediate_name, identification_dict,
+        start_date, end_date and data.
 
-    #     Args:
-    #         manager_name (str): The name of the manager.
-    #         group_id (int): The group ID.
-    #         data (object): The data to be stored in the cache as a Python object.
-    #         date (datetime.datetime): The date for the cache entry.
+        Args:
+            intermediate_name (str): The intermediate name.
+            identification_dict (dict): The identification dictionary.
+            data (object): The data to be cached.
+            start_date (datetime): The start datetime of the cache data.
+            end_date (datetime | None): The end datetime of the cache data.
+        """
+        entry, _ = cls.objects.get_or_create(
+            intermediate_name=intermediate_name,
+            identification=cls.getIdString(identification_dict),
+            start_date=start_date,
+            end_date=end_date
+        )
+        entry.data=pickle.dumps(data)
+        entry.save()
 
-    #     Returns:
-    #         None
-    #     """
-    #     entry, newly_created = cls.objects.get_or_create(
-    #         manager_name=manager_name,
-    #         group_id=group_id,
-    #         date=date
-    #     )
-    #     entry.data=pickle.dumps(data)
-    #     entry.save()
+    @staticmethod
+    def getIdString(identification_dict: dict) -> str:
+        """
+        Convert an identification_dict into a JSON string, with keys sorted.
+
+        Args:
+            identification_dict (dict): The identification dictionary.
+
+        Returns:
+            str: The sorted JSON string of the identification dictionary.
+        """
+        return json.dumps(identification_dict, sort_keys=True)
+
 
 class ProjectGroup(GroupTable):
     """
@@ -541,3 +566,24 @@ class ProjectStaffCost(DataTable):
 
     def __str__(self):
         return f'Project_staff_cost {self.id}'
+
+
+class ScenarioGroup(GroupTable):
+    """
+    A Django model representing a scenario group.
+    """
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+
+class Scenario(DataTable):
+    """
+    A Django model representing a scenario, including its name, data, and associated scenario group.
+    """
+    name = models.CharField(max_length=255)
+    data = models.JSONField()
+
+    def __str__(self):
+        return self.name
