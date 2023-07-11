a569d680c0adecd281da24141072d46f9851536d
ElidaVehapi <115171061+ElidaVehapi@users.noreply.github.com>
2023-07-11 12:54:45 +0000

    debugged FileManagerTest

--- orm_test/backend/test/test_manager/test_file_manager.py
+++ orm_test/backend/test/test_manager/test_file_manager.py
@@ -4,11 +4,15 @@ from PIL import Image
 from backend.models import File, FileExtension, FileGroup, User, FileType
 from backend.src.manager.file_manager import FileManager   
 
-
 class FileManagerTest(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(microsoft_id = 'b')
+        User.objects.all().delete()
+        self.user = User.objects.create(
+            microsoft_id = '12345',
+            first_name = 'Tim'
+        )
+
         self.group_id = 1 
         self.creator_user_id = self.user.id
         self.file_group = FileGroup.objects.create(name = 'fileGroup')
@@ -36,9 +40,7 @@ class FileManagerTest(TestCase):
             }
         self.file_manager_obj = FileManager.create(self.creator_user_id, **kwargs)
 
-        
     def test_create(self):
-
         kwargs = {
             'file_type': self.file_type,
             'data' : self.image_data,
@@ -54,31 +56,31 @@ class FileManagerTest(TestCase):
         self.assertEqual(obj.file_type.id, self.file_type.id)
         self.assertEqual(obj.file_group.id, 3)
     
-    # def test_update(self): 
-    #     update = {
-    #         'data': self.image_data_updated,   
-    #     }
-    #     file_manager = FileManager(self.file_manager_obj.file_group)
-    #     file_manager.update(self.creator_user_id, **update)
+    def test_update(self): 
+        update = {
+            'data': self.image_data_updated,   
+        }
+        file_manager = FileManager(self.file_manager_obj.file_group)
+        file_manager.update(self.creator_user_id, **update)
 
-    #     updated_obj = File.objects.latest('id')
-    #     self.assertEqual(updated_obj.data, self.image_data_updated)
+        updated_obj = File.objects.latest('id')
+        self.assertEqual(updated_obj.data, self.image_data_updated)
 
-    # def test_deactivate(self):
-    #     file_manager = FileManager(self.file_manager_obj.file_group)
-    #     obj = File.objects.get(id=self.file_manager_obj.id)
-    #     self.assertTrue(obj.active)
+    def test_deactivate(self):
+        file_manager = FileManager(self.file_manager_obj.file_group)
+        obj = File.objects.get(id=self.file_manager_obj.id)
+        self.assertTrue(obj.active)
 
-    #     file_manager.deactivate(self.creator_user_id)
-    #     deactivated_obj = File.objects.latest('id')
-    #     self.assertFalse(deactivated_obj.active)
+        file_manager.deactivate(self.creator_user_id)
+        deactivated_obj = File.objects.latest('id')
+        self.assertFalse(deactivated_obj.active)
    
-    # def test_filter(self):
-    #     filter_kwargs = {
-    #         'data': self.image_data,
-    #     }
-    #     result = FileManager.filter(**filter_kwargs)
-    #     expected_result = [
-    #         FileManager(file_group_id=self.file_manager_obj.file_group) 
-    #     ]
-    #     self.assertEqual(result, expected_result)   
+    def test_filter(self):
+        filter_kwargs = {
+            'data': self.image_data,
+        }
+        result = FileManager.filter(**filter_kwargs)
+        expected_result = [
+            FileManager(file_group_id=self.file_manager_obj.file_group) 
+        ]
+        self.assertEqual(result, expected_result)
