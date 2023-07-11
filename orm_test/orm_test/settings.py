f54c8cd0bde27a235fb5852e304f1ed3028ff500
ElidaVehapi <115171061+ElidaVehapi@users.noreply.github.com>
2023-07-11 10:03:22 +0000

    added path for extension for graphik tool

--- orm_test/orm_test/settings.py
+++ orm_test/orm_test/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend'
+    'backend',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
