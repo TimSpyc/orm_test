67471e2c317a987cf538e979385a2a3888c32386
Tim Kleindick <89387156+TK-Constellium@users.noreply.github.com>
2023-08-19 10:08:33 +0000

    clear cache function

--- orm_test/backend/src/auxiliary/db.py
+++ orm_test/backend/src/auxiliary/db.py
@@ -1,4 +1,11 @@
 from django.core.management import call_command
+from backend.models import CacheManager, CacheIntermediate
+from django.core.cache import cache
 
 def truncate_all_tables():
     call_command('flush', '--noinput', reset_sequences=True, verbosity=0)
+
+def clear_cache():
+    CacheManager.objects.all().delete()
+    CacheIntermediate.objects.all().delete()
+    cache.clear()
