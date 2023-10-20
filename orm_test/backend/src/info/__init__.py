d183a4104a7c44129e2cff7e03ca47bf75229450
HildebrandAlice <91737788+HildebrandAlice@users.noreply.github.com>
2023-10-20 12:50:44 +0000

    remove ChangeRequestKeyInfoInfo

--- orm_test/backend/src/info/__init__.py
+++ orm_test/backend/src/info/__init__.py
@@ -2,6 +2,5 @@ from .derivative_constellium_info import DerivativeConstelliumInfo
 from .project_info import ProjectInfo
 from .project_staff_cost_info import ProjectStaffCostInfo
 from .change_request_info import ChangeRequestSelectionInfo, \
-    ChangeRequestKeyInfoInfo, ChangeRequestSendKickOffEmailInfo, \
-    ChangeRequestCostInfo, ChangeRequestFeasibilityInfo, \
-    ChangeRequestRiskInfo, ChangeRequestFileInfo
+    ChangeRequestSendKickOffEmailInfo, ChangeRequestCostInfo, \
+    ChangeRequestFeasibilityInfo, ChangeRequestRiskInfo, ChangeRequestFileInfo
