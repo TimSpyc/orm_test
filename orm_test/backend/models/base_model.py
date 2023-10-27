51bd3413ab4c69b48a7693f7c7015c43a1b2c4bf
MaximilianKelm <127893141+MaximilianKelm@users.noreply.github.com>
2023-10-27 14:04:38 +0000

    Adjust base model to new validator logic

--- orm_test/backend/models/base_model.py
+++ orm_test/backend/models/base_model.py
@@ -3,21 +3,20 @@ from django.db import models
 from django.db.models.fields import NOT_PROVIDED
 from django.core.exceptions import ValidationError
 
+# TODO: Check if there is only one validator per field
 class BaseModel(models.Model):
-    @property
-    def custom_validator_list(self):
-        return NOT_PROVIDED
+    validator_list = NOT_PROVIDED
 
     def clean(self) -> None:
-        if self.custom_validator_list == NOT_PROVIDED\
-        or not self.custom_validator_list:
+        if self.validator_list == NOT_PROVIDED\
+        or not self.validator_list:
             return
 
         validation_error_list = []
 
-        for custom_validator_obj in self.custom_validator_list:
+        for custom_validator_obj in self.validator_list:
             try:
-                custom_validator_obj()
+                custom_validator_obj(self).validate()
             except ValidationError as e:
                 validation_error_list.append(e)
             except Exception as e:
