24f034b3404a7f7a0a5f24fe2b2a183e639816ae
MK-Constellium <91201444+MK-Constellium@users.noreply.github.com>
2023-11-07 11:28:26 +0000

    Delete scenario models, manager and info

--- orm_test/backend/src/info/scenario_info.py
+++
@@ -1,8 +0,0 @@
-from backend.src.auxiliary.info import GeneralInfo
-from backend.src.manager import ScenarioManager
-
-class ScenarioInfo(GeneralInfo):
-    base_url = 'scenario'
-    allowed_method_list = ['GET_detail', 'GET_list', 'POST', 'PUT', 'DELETE']
-    required_permission_list = []
-    manager = ScenarioManager
